b16a54a6e77bef3d09d8a1cac94f5d16cb0e63a6
geoom <gmejia.fisi@gmail.com>
2014-02-01 02:00:54 -0500

    make sure that email address is associate with a person

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe EmailAddress do
   let(:email_address) do
-  	EmailAddress.new(address: 'hi.geoom@gmail.com')
+  	EmailAddress.new(address: 'hi.geoom@gmail.com', person_id: '1')
   end
   it 'is valid' do
   	expect(email_address).to be_valid
@@ -10,5 +10,9 @@ describe EmailAddress do
   it 'is invalid without address' do
   	email_address.address = nil
   	expect(email_address).to_not be_valid
+  end
+  it 'must have a reference to a person' do
+    email_address.person_id = nil
+    expect(email_address).to_not be_valid
   end
 end
