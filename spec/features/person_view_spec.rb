5d2310e2a5d7638387d3f9a6cdb18443dfa712fb
geoom <gmejia.fisi@gmail.com>
2014-02-01 02:31:52 -0500

    integration test that verify emails list and add email address link

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -5,61 +5,87 @@ require 'capybara/rspec'
 describe 'the person view', type: :feature do
 	let(:person) { Person.create(first_name: 'George', last_name: 'Mejia') }
 
-	before(:each) do
-		person.phone_numbers.create(number: '12345678')
-		person.phone_numbers.create(number: '098765432')
-		visit person_path(person)
-	end
+	describe 'phone numbers' do
 
-	it 'show the phone numbers' do
-		person.phone_numbers.each do |phone|
-			expect(page).to have_content(phone.number)
+		before(:each) do
+			person.phone_numbers.create(number: '12345678')
+			person.phone_numbers.create(number: '098765432')
+			visit person_path(person)
+		end
+
+		it 'show the phone numbers' do
+			person.phone_numbers.each do |phone|
+				expect(page).to have_content(phone.number)
+			end
+		end
+
+		it 'has a link to add new phone number' do
+			expect(page).to have_link('Add phone number', href: new_phone_number_path(person_id: person.id))
+		end
+
+		it 'adds a new phone number' do
+			page.click_link('Add phone number')
+			page.fill_in('Number', with: '12345678')
+			page.click_button('Create Phone number')
+
+			expect(current_path).to eq(person_path(person))
+			expect(page).to have_content('12345678')
+		end
+
+		it 'has links to edit phone numbers' do
+			person.phone_numbers.each do |phone|
+				expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+			end
+		end
+
+		it 'edits a phone number' do
+			phone = person.phone_numbers.first
+			old_number = phone.number
+
+			first(:link, 'edit').click
+			page.fill_in('Number', with: '555-9191')
+			page.click_button('Update Phone number')
+			expect(current_path).to eq(person_path(person))
+			expect(page).to have_content('555-9191')
+			expect(page).to_not have_content(old_number)
+		end
+
+		it 'has link to delete a phone number' do
+			person.phone_numbers.each do |phone|
+				expect(page).to have_link('remove', href: phone_number_path(phone))
+			end
+		end
+
+		it 'removes a phone number' do
+			phone = person.phone_numbers.first
+			old_number = phone.number
+
+			first(:link, 'remove').click
+			expect(current_path).to eq(person_path(person))
+			expect(page).to_not have_link('remove', href: phone_number_path(old_number))
 		end
 	end
 
-	it 'has a link to add new phone number' do
-		expect(page).to have_link('Add phone number', href: new_phone_number_path(person_id: person.id))
-	end
+	describe 'email addresses' do
+		
+		before(:each) do
+			person.email_addresses.create(address: 'email@email.com')
+			person.email_addresses.create(address: 'email@email.com')
+			visit person_path(person)
+		end
 
-	it 'adds a new phone number' do
-		page.click_link('Add phone number')
-		page.fill_in('Number', with: '12345678')
-		page.click_button('Create Phone number')
+		it 'show email addresses' do
+			person.email_addresses.each do |email|
+				expect(page).to have_selector('li', text: email.address)
+			end
+		end
 
-		expect(current_path).to eq(person_path(person))
-		expect(page).to have_content('12345678')
-	end
-
-	it 'has links to edit phone numbers' do
-		person.phone_numbers.each do |phone|
-			expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+		it 'has a add email address link' do
+			expect(page).to have_link('Add email address', href: new_email_address_path(person_id: person.id))
 		end
 	end
 
-	it 'edits a phone number' do
-		phone = person.phone_numbers.first
-		old_number = phone.number
 
-		first(:link, 'edit').click
-		page.fill_in('Number', with: '555-9191')
-		page.click_button('Update Phone number')
-		expect(current_path).to eq(person_path(person))
-		expect(page).to have_content('555-9191')
-		expect(page).to_not have_content(old_number)
-	end
 
-	it 'has link to delete a phone number' do
-		person.phone_numbers.each do |phone|
-			expect(page).to have_link('remove', href: phone_number_path(phone))
-		end
-	end
 
-	it 'removes a phone number' do
-		phone = person.phone_numbers.first
-		old_number = phone.number
-
-		first(:link, 'remove').click
-		expect(current_path).to eq(person_path(person))
-		expect(page).to_not have_link('remove', href: phone_number_path(old_number))
-	end
 end
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  let(:email_address) do
-  	EmailAddress.new(address: 'hi.geoom@gmail.com', person_id: '1')
-  end
-  it 'is valid' do
-  	expect(email_address).to be_valid
-  end
-  it 'is invalid without address' do
-  	email_address.address = nil
-  	expect(email_address).to_not be_valid
-  end
-  it 'must have a reference to a person' do
-    email_address.person_id = nil
-    expect(email_address).to_not be_valid
-  end
+   let(:email_address) do
+      EmailAddress.new(address: 'hi.geoom@gmail.com', person_id: '1')
+   end
+   it 'is valid' do
+      expect(email_address).to be_valid
+   end
+   it 'is invalid without address' do
+      email_address.address = nil
+      expect(email_address).to_not be_valid
+   end
+   it 'must have a reference to a person' do
+      email_address.person_id = nil
+      expect(email_address).to_not be_valid
+   end
 end
